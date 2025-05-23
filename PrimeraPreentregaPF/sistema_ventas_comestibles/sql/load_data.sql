a159e7555556167edbc7be6bebe7e5791bf809e2
Romina Cattaneo <83126622+romica44@users.noreply.github.com>
2025-05-23 17:35:16 -0300

    load data y requirements

--- PrimeraPreentregaPF/sistema_ventas_comestibles/sql/load_data.sql
+++ PrimeraPreentregaPF/sistema_ventas_comestibles/sql/load_data.sql
@@ -13,9 +13,8 @@ TRUNCATE TABLE cities;
 TRUNCATE TABLE countries;
 
 -- Cargar datos desde archivos CSV
--- NOTA: Ajustar las rutas según tu sistema operativo
 
-LOAD DATA INFILE 'C:/ruta/completa/al/proyecto/data/countries.csv'
+LOAD DATA INFILE 'C:/Users/Home/Desktop/Data_Engineer/PrimeraPreentregaPF/sistema_ventas_comestibles/data/countries.csv'
 INTO TABLE countries
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -23,7 +22,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (CountryID, CountryName, CountryCode);
 
-LOAD DATA INFILE 'C:/ruta/completa/al/proyecto/data/cities.csv'
+LOAD DATA INFILE 'C:/Users/Home/Desktop/Data_Engineer/PrimeraPreentregaPF/sistema_ventas_comestibles/data/cities.csv'
 INTO TABLE cities
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -31,7 +30,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (CityID, CityName, Zipcode, CountryID, countries_CountryID);
 
-LOAD DATA INFILE 'C:/ruta/completa/al/proyecto/data/categories.csv'
+LOAD DATA INFILE 'C:/Users/Home/Desktop/Data_Engineer/PrimeraPreentregaPF/sistema_ventas_comestibles/data/categories.csv'
 INTO TABLE categories
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -39,7 +38,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (CategoryID, CategoryName);
 
-LOAD DATA INFILE 'C:/ruta/completa/al/proyecto/data/products.csv'
+LOAD DATA INFILE 'C:/Users/Home/Desktop/Data_Engineer/PrimeraPreentregaPF/sistema_ventas_comestibles/data/products.csv'
 INTO TABLE products
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -47,7 +46,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (ProductID, ProductName, Price, CategoryID, Class, ModifyDate, Resistant, JuMiergc, VariableDate);
 
-LOAD DATA INFILE 'C:/ruta/completa/al/proyecto/data/customers.csv'
+LOAD DATA INFILE 'C:/Users/Home/Desktop/Data_Engineer/PrimeraPreentregaPF/sistema_ventas_comestibles/data/customers.csv'
 INTO TABLE customers
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -55,7 +54,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (CustomerID, FirstName, MiddleInitial, LastName, CityID, Address, cities_CityID, cities_countries_CountryID);
 
-LOAD DATA INFILE 'C:/ruta/completa/al/proyecto/data/employees.csv'
+LOAD DATA INFILE 'C:/Users/Home/Desktop/Data_Engineer/PrimeraPreentregaPF/sistema_ventas_comestibles/data/employees.csv'
 INTO TABLE employees
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -68,7 +67,7 @@ SET
     sales_customers_cities_CityID = NULLIF(@sales_customers_cities_CityID, ''),
     sales_customers_cities_countries_CountryID = NULLIF(@sales_customers_cities_countries_CountryID, '');
 
-LOAD DATA INFILE 'C:/ruta/completa/al/proyecto/data/sales.csv'
+LOAD DATA INFILE 'C:/Users/Home/Desktop/Data_Engineer/PrimeraPreentregaPF/sistema_ventas_comestibles/data/sales.csv'
 INTO TABLE sales
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
